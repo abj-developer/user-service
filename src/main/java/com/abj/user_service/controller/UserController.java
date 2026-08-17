45ecb8b825ed0ab8cf781617f90840857a1048e3
ambuj <srivastavvaambuj42@gmail.com>
2026-08-17 18:00:25 +0530

    added swagger for user service

--- src/main/java/com/abj/user_service/controller/UserController.java
+++ src/main/java/com/abj/user_service/controller/UserController.java
@@ -3,24 +3,61 @@ package com.abj.user_service.controller;
 import com.abj.user_service.VO.ResponseTemplateVO;
 import com.abj.user_service.entity.User;
 import com.abj.user_service.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "User APIs",
+        description = "APIs related to user management"
+)
 @RestController
 @RequestMapping("/users")
 @Slf4j
+
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(
+            summary = "save User",
+            description = "saves a new user"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "User saved successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid User data"
+            )
+    })
     @PostMapping("/")
     public User saveUser(@RequestBody User user){
         log.info("inside saveUser method of UserController");
         return userService.saveUser(user);
     }
 
+    @Operation(
+            summary = "Get user by ID",
+            description = "Fetches user details using the user ID"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User found successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "User not found"
+            )
+    })
     @GetMapping("/{id}")
     public ResponseTemplateVO getUserWitDepartment(@PathVariable("id") Long userId){
         log.info("inside getUserWitDepartment method of UserController");
--- src/main/java/com/abj/user_service/entity/User.java
+++ src/main/java/com/abj/user_service/entity/User.java
@@ -1,5 +1,6 @@
 package com.abj.user_service.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,18 +11,39 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
+@Schema(description = "User information")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Schema(
+            description = "Unique identifier of user",
+            example = "101"
+    )
     private Long userId;
 
+    @Schema(
+            description = "User's first name",
+            example = "Rahul"
+    )
     private String firstName;
 
+    @Schema(
+            description = "User's last name",
+            example = "Sharma"
+    )
     private String lastName;
 
+    @Schema(
+            description = "User email address",
+            example = "rahul@example.com"
+    )
     private String email;
 
+    @Schema(
+            description = "Department to which user belongs",
+            example = "101"
+    )
     private Long departmentId;
 
 }
